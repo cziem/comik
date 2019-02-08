75983eb80bf52f390c19a1f3d1b81145b248aede
Fayvor George <phavorsparks@gmail.com>
2019-02-08 17:37:13 +0100

    try out the api

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require('../config/config')
+const crypto = require('crypto')
+const axios = require('axios')
+
+
+const key = process.env.API_KEY
+const privKey = process.env.PRIV_KEY
+let ts = Date.now()
+const hash = crypto.createHash('md5').update(ts + privKey + key).digest('hex')
+const uri = `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${key}&hash=${hash}`
+
+axios
+  .get(uri)
+  .then(blob => 
+    console.log(blob.data.data)
+  )
+  .catch(err => console.log(err))
